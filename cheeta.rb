8b377e2b89ff76c7e33bedf8b6238ba37116ced2
Michael <miguelmmm809@gmail.com>
2016-04-11 15:51:04 -0400

    Update cheeta.rb

---
+++ cheeta.rb
@@ -0,0 +1,29 @@
+```ruby
+CHEETAH_GIRLS = [
+  "Raven-Symone",
+  "Adrienne Bailon",
+  "Sabrina Bryan",
+  "Kiely Williams"
+]
+
+auditions = [
+  "Raven-Symone",
+  "Usher",
+  "Wiz Khalifa",
+  "Adrienne Bailon",
+  "Hulk Hogan",
+  "Sabrina Bryan",
+  "Diego Lugo",
+  "Kiely Williams",
+  "Justin Timberlake"
+]
+
+panel = ["Raven-Symone","Adrienne Bailon","Sabrina Bryan","Kiely Williams"]
+if auditions.include? CHEETAH_GIRLS
+
+  puts "#{panel}"
+
+end
+
+puts panel
+```
